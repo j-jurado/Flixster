dae89320c08dab80c2d6d62575c5194e24dc35de
j-jurado <73544267+j-jurado@users.noreply.github.com>
2021-09-16 16:25:13 -0400

    Detail View and Grid View
    Implemented a details view page after selecting a page. Implemented a grid view with the superhero "similar" movies tab

---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Jesus Jurado on 9/16/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
--- Flixster/MoviesViewController.swift
+++ Flixster/MoviesViewController.swift
@@ -39,9 +39,6 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
                 self.movies = dataDictionary["results"] as! [[String:Any]]
                 
                 self.tableView.reloadData()
-                
-                print(dataDictionary)
-                
              }
         }
         task.resume()
@@ -72,13 +69,25 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     
 
     /*
-    // MARK: - Navigation
+     MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
+     In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     */
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+         
+        print("Loading up the details screen")
+        
+        // Find the selected movie
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        // Pass the selected movie to the details view controller
+        let detailViewController = segue.destination as! MovieDetailsViewController
+        detailViewController.movie = movie
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 
 }
